3c5d9e894ac31736cab34791e51b23dcec6c7627
leon baez <baez.requiem@gmail.com>
2022-10-21 16:40:20 -0300

    feat: add router dependence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Routes, Route } from "react-router-dom"
+
 import { HomePage } from "./pages"
 
 function App() {
 
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
-import { ThemeProvider } from 'styled-components'
+import App from './App'
 
 import theme from './theme'
 import GlobalStyles from './styles/globalStyles'
+
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 )
